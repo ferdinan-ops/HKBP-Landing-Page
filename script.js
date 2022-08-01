553f71f686f5daea5f29937f6db7bb376fcacca1
ferdinan-ops <perdinantumanggor95@gmail.com>
2022-08-01 22:03:29 +0700

    menu

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const menu = document.querySelector(".menu");
+const link = document.querySelector(".menu ul.link");
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
+  link.style.top = "70px";
+});
+
+menu.addEventListener("click", () => {
+  link.classList.toggle("active");
 });
